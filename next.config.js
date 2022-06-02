7693b89b6062372057db1bdf39beb2025ea7764e
Alexander Angulo <alex.anguloh@gmail.com>
2022-06-02 15:40:11 -0500

    FEAT: Add pwa settings

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
+const withPWA = require("next-pwa");
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   images: {
     domains: ['images.unsplash.com', 'source.unsplash.com', 'iconarchive.com'],
   },
-}
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === 'development',
+  },
+});
 
 module.exports = nextConfig
